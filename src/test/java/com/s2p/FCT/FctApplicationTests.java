c6c318a20299557f342268bf2cb6b2380a9f9060
mdevnagarale <mahadevnagarale97@gmail.com>
2025-04-26 13:52:57 +0530

    first commit

---
+++ src/test/java/com/s2p/FCT/FctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2p.FCT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
