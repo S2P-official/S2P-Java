6252de23165ef5e6966a3b9abd01f2af0da41993
santoshkarpurashetti <karpurashettis@gmail.com>
2025-05-14 13:35:54 +0530

    14-05-25

--- src/main/java/com/s2p/FCT/controllers/ProductController.java
+++ src/main/java/com/s2p/FCT/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.s2p.FCT.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.s2p.FCT.entity.Product;
 import com.s2p.FCT.services.ProductService;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController()
 public class ProductController {
 
