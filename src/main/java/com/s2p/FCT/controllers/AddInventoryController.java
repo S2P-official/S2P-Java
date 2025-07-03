b8c200e17b82378c8bb6f918a12183d810584ab0
santoshkarpurashetti <karpurashettis@gmail.com>
2025-07-03 19:41:29 +0530

    cors origion

--- src/main/java/com/s2p/FCT/controllers/AddInventoryController.java
+++ src/main/java/com/s2p/FCT/controllers/AddInventoryController.java
@@ -6,15 +6,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.s2p.FCT.entity.AddInventory;
 import com.s2p.FCT.services.AddInventoryService;
-
-@Controller
+@CrossOrigin(origins = "*")
+@RestController
 public class AddInventoryController {
 	 @Autowired
 
