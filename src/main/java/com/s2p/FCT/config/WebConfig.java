d53c6501f8fe2bf6fcb920bdc42ceda4101394c4
santoshkarpurashetti <karpurashettis@gmail.com>
2025-07-04 10:51:09 +0530

    Cors Polycy update

--- src/main/java/com/s2p/FCT/config/WebConfig.java
+++ src/main/java/com/s2p/FCT/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.s2p.FCT.config; // 👈 change this to match your package
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/"); // folder where images are stored
+    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // allow all paths
+                .allowedOrigins("*") // or specific: "http://localhost:3000"
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
